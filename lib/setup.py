a47803c09953904b00b4f9e4c634d4cb060752ec
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2015-05-30 20:28:10 -0400

    Adding a bit more info to the setup.

--- lib/setup.py
+++ lib/setup.py
@@ -1,11 +1,21 @@
+import os
+import sys
 from setuptools import setup
+import beach
 
 setup( name = 'beach',
-       version = '0.1',
+       version = beach.__version__,
        description = 'Simple private python cloud framework',
        url = 'https://github.com/refractionPOINT/py-beach',
        author = 'refractionPOINT',
        author_email = 'maxime@refractionpoint.com',
        license = 'GPLv2',
        packages = [ 'beach' ],
-       zip_safe = False )
+       zip_safe = False,
+       install_requires = [ 'libzmq',
+                            'python-gevent',
+                            'pyzmq',
+                            'netifaces',
+                            'pyyaml',
+                            'psutil' ],
+       long_description = 'Python private compute cloud framework with a focus on ease of deployment and expansion rather than pure performance.' )
