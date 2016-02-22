fd224b219286016b1b841c620d7928150e985539
Maxime Lamothe-Brassard <maximelb@google.com>
2016-02-22 12:06:59 -0800

    Adding licensing banner.

--- beach/patrol.py
+++ beach/patrol.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2015  refractionPOINT
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+'''Daemon used to ensure that Actors of a certain setup stay available in the cluster.'''
+
+
 from beach.beach_api import Beach
 import logging
 import logging.handlers
